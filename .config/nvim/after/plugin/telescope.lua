3aa6706f3c5b580474d060fd9972a901f074e4f3
Narhari Motivaras <narhari.motivaras76@gmail.com>
2025-03-25 11:43:01 +0530

    playing with telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,20 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', function() 
+    builtin.find_files({ hidden = true });
+end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>fg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>lf', function()
+    builtin.lsp_document_symbols({
+        symbols = { 'function', 'method' }
+    })
+end)
 
+vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+
+-- This is open neovim config dir from active dir
+vim.keymap.set('n', '<leader>gc', function() 
+    builtin.find_files({cwd=vim.fn.stdpath("config")})
+end)
